2c11499595f250cd46caec8c6f802122111184d1
Juan Francisco Rodriguez Vazquez <jfrodriguezv@truper.com>
2026-09-25 14:20:23 -0600

    Deploy: Revela el Paisaje con shader (sin cuadricula) + pluma

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790366562|2828782';
+const CACHE_VERSION = '1790367614|3185837';
 /** @type {string} */
 const CACHE_PREFIX = 'Arcade Platform-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
